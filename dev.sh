bec7f0c367126adcd63bbd39a125c60d887e44b0
jeroenpeeters <jeroen@peetersweb.nl>
2016-03-01 08:36:44 +0100

    simple bash script to start the project in development mode using containers. To run fleetr for development it is not needed to have Meteor installed.

---
+++ dev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker run --name mongo-dev -d mongo
+docker run --name fleetr-dev -it --rm -p 3000:3000 -v "$(pwd)":/app --link mongo-dev:db -e "MONGO_URL=mongodb://db" danieldent/meteor
+
