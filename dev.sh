fc6255b7f83a0add33637b80ac3c86484dfd4368
jeroenpeeters <jeroen@peetersweb.nl>
2016-03-05 12:41:06 +0100

    dev script

--- dev.sh
+++ dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --name mongo-dev -d mongo
-docker run --name fleetr-dev -it --rm -p 3000:3000 -v "$(pwd)":/app --link mongo-dev:db -e "MONGO_URL=mongodb://db" danieldent/meteor
+docker run --name mongo-dev -d mongo || docker start mongo-dev
+docker rm fleetr-dev; docker run --name fleetr-dev -it --rm -p 3000:3000 -v "$(pwd)":/app --link mongo-dev:db -e "MONGO_URL=mongodb://db" danieldent/meteor
 
