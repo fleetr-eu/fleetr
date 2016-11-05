0938d95b2b138b862696bbadb457e75b5948fb1c
Gantcho Kojuharov <gantcho@greyarch.net>
2016-11-05 14:06:53 +0100

    fixed local build process

--- build.sh
+++ build.sh
@@ -1,15 +1,7 @@
-#!/bin/sh
-# docker run --rm -it -v ${PWD}:/source -v /tmp:/build greyarch/meteor-build \
-#   bash -c "rm -rf node_modules; npm i --production; \
-#           meteor build /build --directory --unsafe-perm --server-only --architecture os.linux.x86_64; \
-#           cp /source/Dockerfile.prod /build/bundle/Dockerfile; \
-#           cp /source/package.json /build/bundle/"
-# rm -rf node_modules
-# npm i --production
-meteor build /tmp/fleetr-build --directory --server-only --architecture os.linux.x86_64
-cd /tmp/fleetr-build/bundle/programs/server
-npm i --production
+#!/bin/bash
+set -e
+meteor build /tmp/fleetr-build --unsafe-perm --directory --server-only --architecture os.linux.x86_64
+(cd /tmp/fleetr-build/bundle/programs/server && npm i --production)
 cp Dockerfile /tmp/fleetr-build/
-cd /tmp/fleetr-build
-docker build -t fleetr/web .
+docker build -t fleetr/web -f /tmp/fleetr-build
 docker push fleetr/web
