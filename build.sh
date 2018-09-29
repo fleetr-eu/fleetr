8f61fcdd2680a0721c9e69549af612d9c28a9a0b
Gantcho Kojuharov <gantcho@greyarch.net>
2018-09-29 13:30:28 +0200

    replace circleci build with multistage docker hub build

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-npm i --production
-meteor build /tmp/fleetr-build --directory --server-only --architecture os.linux.x86_64
-cp Dockerfile.prod /tmp/fleetr-build/Dockerfile
-docker build -t fleetr/web /tmp/fleetr-build
-docker push fleetr/web
